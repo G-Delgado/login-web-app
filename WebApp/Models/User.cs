b21eb74947f0113aa4f76fee90f1abc7e1f9ff85
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-02-05 10:58:43 -0500

    First Commit

---
+++ WebApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+    }
+}
